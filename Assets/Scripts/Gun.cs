f5211eb068a64ef29d79552962a58b53b3a1abe9
Nick Bykon <nikita.bikon@gmail.com>
2020-12-20 10:55:20 +0200

    Add VR project

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float speed = 40f;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void Fire()
+    {
+        GameObject spawneBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spawneBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawneBullet, 2f);
+    }
+}
---
+++ Assets/Scripts/XROffsetGrabInteractable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class XROffsetGrabInteractable : XRGrabInteractable
+{
+    private Vector3 initionalAttachLocalPos;
+    private Quaternion initionalAttachLocalRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create attach point
+        if(!attachTransform)
+        {
+            GameObject grab = new GameObject("Grab Pivot");
+            grab.transform.SetParent(transform, false);
+            attachTransform = grab.transform;
+        }
+
+        initionalAttachLocalPos = attachTransform.localPosition;
+        initionalAttachLocalRot = attachTransform.localRotation;
+    }
+
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        if(interactor is XRDirectInteractor)
+        {
+            attachTransform.position = interactor.transform.position;
+            attachTransform.rotation = interactor.transform.rotation;
+        }
+        else
+        {
+            attachTransform.localPosition = initionalAttachLocalPos;
+            attachTransform.localRotation = initionalAttachLocalRot;
+        }
+        base.OnSelectEntered(interactor);
+    }
+}
