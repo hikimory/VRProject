7a08d79b321406e0a38951c23b84042ec5560523
Nick Bykon <nikita.bikon@gmail.com>
2020-12-21 17:48:29 +0200

    Add Ray Interaction

--- Assets/Scripts/LocomotionController.cs
+++ Assets/Scripts/LocomotionController.cs
@@ -10,20 +10,30 @@ public class LocomotionController : MonoBehaviour
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
 
+    public XRRayInteractor leftInteractorRay;
+    public XRRayInteractor rightInteractorRay;
+
     public bool EnableLeftTeleport { get; set;} = true;
     public bool EnableRightTeleport { get; set;} = true;
     
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = new Vector3();
+        Vector3 norm = new Vector3();
+        int index = 0;
+        bool validTarget = false;
+
         if(leftTeleportRay)
         {
-            leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay));
+            bool isLeftIneractorRayHovering = leftInteractorRay.TryGetHitInfo(ref pos, ref norm, ref index, ref validTarget);
+            leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay) && !isLeftIneractorRayHovering );
         }
 
         if(rightTeleportRay)
         {
-            rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay));
+            bool isRightIneractorRayHovering = rightInteractorRay.TryGetHitInfo(ref pos, ref norm, ref index, ref validTarget);
+            rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay) && !isRightIneractorRayHovering);
         }
     }
 
