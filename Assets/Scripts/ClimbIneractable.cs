3df2db77793ace22a199311ae89ddacc93779e5d
Nick Bykon <nikita.bikon@gmail.com>
2020-12-22 14:15:28 +0200

    Add Climbing

---
+++ Assets/Scripts/ClimbIneractable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClimbIneractable : XRBaseInteractable
+{
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+
+        if(interactor is XRDirectInteractor)
+            Climber.climbingHand = interactor.GetComponent<XRController>();
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+
+        if(interactor is XRDirectInteractor)
+        {
+            if(Climber.climbingHand && Climber.climbingHand.name == interactor.name)
+            {
+                Climber.climbingHand = null;
+            }
+        }
+    }
+
+}
